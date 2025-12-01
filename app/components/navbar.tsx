08a242e7fe9b6b36344ce76bbf78644f11c8e06e
SP <threehappypenguins@gmail.com>
2025-12-01 13:08:24 -0400

    Fixed flex mistake

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -16,7 +16,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo/Name */}
-          <div className="flex-0">
+          <div className="shrink-0">
             <Link href="/" className="text-xl font-bold">
               Sarah Poulin
             </Link>
